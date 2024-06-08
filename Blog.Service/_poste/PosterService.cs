cd4d7c6ca9847ab2e65d7c452a07f73baacff42f
toiQS <88361704+toiQS@users.noreply.github.com>
2024-06-08 17:53:59 +0700

    Update PosterService.cs

--- Blog.Service/_poste/PosterService.cs
+++ Blog.Service/_poste/PosterService.cs
@@ -3,11 +3,8 @@ using Blog_Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.Contracts;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Blog.Service._poste
@@ -16,17 +13,23 @@ namespace Blog.Service._poste
     {
         private readonly ApplicationDbContext _context;
         private readonly ILogger<PosterService> _logger;
+
         public PosterService(ApplicationDbContext context, ILogger<PosterService> logger)
         {
-            _context = context;
-            _logger = logger;
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
+
+        /// <summary>
+        /// Retrieves all posters.
+        /// </summary>
+        /// <returns>A list of all posters.</returns>
         public async Task<ServiceResult<IEnumerable<PosterResponse>>> GetAll()
         {
             try
             {
-                var getData = await _context.Posters
-                    .Select(x => new PosterResponse()
+                var posters = await _context.Posters
+                    .Select(x => new PosterResponse
                     {
                         Intro = x.Intro,
                         PosterID = x.PosterID,
@@ -40,25 +43,33 @@ namespace Blog.Service._poste
                         }
                     })
                     .ToArrayAsync();
-                if (getData != null)
-                {
-                    return ServiceResult<IEnumerable<PosterResponse>>.SuccessResult(getData);
-                }
-                throw new Exception("Not Found or Data don't exist");
+
+                return posters != null
+                    ? ServiceResult<IEnumerable<PosterResponse>>.SuccessResult(posters)
+                    : throw new Exception("No posters found.");
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Error retrieving all posters.");
                 return ServiceResult<IEnumerable<PosterResponse>>.FailedResult(ex.Message);
             }
         }
+
+        /// <summary>
+        /// Retrieves posters that match the given text.
+        /// </summary>
+        /// <param name="text">The text to search for.</param>
+        /// <returns>A list of posters matching the text.</returns>
         public async Task<ServiceResult<IEnumerable<PosterResponse>>> GetByText(string text)
         {
-            if (string.IsNullOrEmpty(text)) throw new ArgumentNullException(text);
+            if (string.IsNullOrEmpty(text))
+                throw new ArgumentNullException(nameof(text));
+
             try
             {
-                var getData = await _context.Posters
+                var posters = await _context.Posters
                     .Where(x => x.Title.ToLower().Contains(text.ToLower()))
-                    .Select(x => new PosterResponse()
+                    .Select(x => new PosterResponse
                     {
                         Intro = x.Intro,
                         PosterID = x.PosterID,
@@ -72,25 +83,33 @@ namespace Blog.Service._poste
                         }
                     })
                     .ToListAsync();
-                if (getData != null)
-                {
-                    return ServiceResult<IEnumerable<PosterResponse>>.SuccessResult(getData);
-                }
-                throw new Exception("Not Found or Data don't exist");
+
+                return posters.Any()
+                    ? ServiceResult<IEnumerable<PosterResponse>>.SuccessResult(posters)
+                    : throw new Exception("No matching posters found.");
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Error retrieving posters by text.");
                 return ServiceResult<IEnumerable<PosterResponse>>.FailedResult(ex.Message);
             }
         }
+
+        /// <summary>
+        /// Retrieves the details of a specific poster by ID.
+        /// </summary>
+        /// <param name="posterID">The ID of the poster.</param>
+        /// <returns>The details of the poster.</returns>
         public async Task<ServiceResult<PosterResponseDetail>> GetDetailByID(int posterID)
         {
-            if (posterID < 0) throw new ArgumentNullException();
+            if (posterID < 0)
+                throw new ArgumentOutOfRangeException(nameof(posterID));
+
             try
             {
-                var getData = await _context.Posters
+                var poster = await _context.Posters
                     .Where(x => x.PosterID == posterID)
-                    .Select(x => new PosterResponseDetail()
+                    .Select(x => new PosterResponseDetail
                     {
                         Intro = x.Intro,
                         PosterContext = x.PosterContext,
@@ -106,30 +125,29 @@ namespace Blog.Service._poste
                         ThemeID = x.ThemeID,
                     })
                     .FirstOrDefaultAsync();
-                if (getData != null)
-                {
-                    return ServiceResult<PosterResponseDetail>.SuccessResult(getData);
-                }
-                throw new Exception("Not Found or Data don't exist");
 
+                return poster != null
+                    ? ServiceResult<PosterResponseDetail>.SuccessResult(poster)
+                    : throw new Exception("Poster not found.");
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Error retrieving poster details by ID.");
                 return ServiceResult<PosterResponseDetail>.FailedResult(ex.Message);
             }
         }
+
+        /// <summary>
+        /// Creates a new poster.
+        /// </summary>
+        /// <param name="poster">The poster data.</param>
+        /// <returns>True if the poster was created successfully.</returns>
         public async Task<ServiceResult<bool>> Create(PosterRequest poster)
         {
-            var checkInput = poster.ToString();
-            if (string.IsNullOrEmpty(checkInput)) throw new ArgumentNullException();
-            var broken = checkInput.Split(",");
-            foreach (var item in broken)
-            {
-                if (string.IsNullOrEmpty(item) || item == "0")
-                    throw new Exception();
-            }
+            if (poster == null)
+                throw new ArgumentNullException(nameof(poster));
 
-            var convertData = new Poster
+            var newPoster = new Poster
             {
                 Intro = poster.Intro,
                 PosterContext = poster.PosterContext,
@@ -137,101 +155,95 @@ namespace Blog.Service._poste
                 UpdateAt = poster.UpdateAt,
                 CreateAt = poster.CreateAt,
                 ThemeID = poster.ThemeID,
-                ImagePoster = new Image()
+                ImagePoster = new Image
                 {
                     ImageType = poster.ImagePoster.ImageType,
                     ImageName = poster.ImagePoster.ImageName,
                     ImageUrl = poster.ImagePoster.ImageUrl,
                 }
             };
+
             try
             {
-                await _context.Posters.AddAsync(convertData);
+                await _context.Posters.AddAsync(newPoster);
                 await _context.SaveChangesAsync();
+
                 return ServiceResult<bool>.SuccessResult(true);
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Error creating new poster.");
                 return ServiceResult<bool>.FailedResult(ex.Message);
             }
         }
+
+        /// <summary>
+        /// Updates an existing poster.
+        /// </summary>
+        /// <param name="posterID">The ID of the poster to update.</param>
+        /// <param name="poster">The updated poster data.</param>
+        /// <returns>True if the poster was updated successfully.</returns>
         public async Task<ServiceResult<bool>> Update(int posterID, PosterRequestUpdate poster)
         {
-            var checkInput = poster.ToString();
-            if (string.IsNullOrEmpty(checkInput)) throw new ArgumentNullException();
-            var broken = checkInput.Split(",");
-            foreach (var item in broken)
-            {
-                if (string.IsNullOrEmpty(item) || item == "0")
-                    throw new Exception();
-            }
-            if (posterID < 0) throw new ArgumentOutOfRangeException();
+            if (posterID < 0)
+                throw new ArgumentOutOfRangeException(nameof(posterID));
+            if (poster == null)
+                throw new ArgumentNullException(nameof(poster));
+
             try
             {
-                var getData = await _context.Posters
-                    .Where(x => x.PosterID == posterID)
-                    .FirstOrDefaultAsync();
+                var existingPoster = await _context.Posters
+                    .FirstOrDefaultAsync(x => x.PosterID == posterID);
 
+                if (existingPoster == null)
+                    throw new Exception("Poster not found.");
 
-                if (getData != null)
-                {
-                    
+                // Update properties if they are different
+                existingPoster.ThemeID = poster.ThemeID;
+                existingPoster.PosterContext = poster.PosterContext;
+                existingPoster.Title = poster.Title;
+                existingPoster.Intro = poster.Intro;
+                existingPoster.UpdateAt = DateTime.Now;
 
-                    if (getData.ThemeID != poster.ThemeID)
-                    {
-                        getData.ThemeID = poster.ThemeID;
-                    }
+                _context.Posters.Update(existingPoster);
+                await _context.SaveChangesAsync();
 
-                    if (getData.PosterContext != poster.PosterContext)
-                    {
-                        getData.PosterContext = poster.PosterContext;
-                    }
-
-                    if (getData.Title != poster.Title)
-                    {
-                        getData.Title = poster.Title;
-                    }
-
-                    if (getData.Intro != poster.Intro)
-                    {
-                        getData.Intro = poster.Intro;
-                    }
-
-                    getData.UpdateAt = DateTime.Now;
-                    _context.Posters.Update(getData);
-                    
-                    await _context.SaveChangesAsync();
-
-                    return ServiceResult<bool>.SuccessResult(true);
-                }
-                throw new Exception("Not Found or Data don't exist");
+                return ServiceResult<bool>.SuccessResult(true);
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Error updating poster.");
                 return ServiceResult<bool>.FailedResult(ex.Message);
             }
         }
+
+        /// <summary>
+        /// Deletes a poster by ID.
+        /// </summary>
+        /// <param name="posterID">The ID of the poster to delete.</param>
+        /// <returns>True if the poster was deleted successfully.</returns>
         public async Task<ServiceResult<bool>> Delete(int posterID)
         {
-            if (posterID < 0) throw new ArgumentOutOfRangeException();
+            if (posterID < 0)
+                throw new ArgumentOutOfRangeException(nameof(posterID));
+
             try
             {
-                var getData = await _context.Posters
-                    .Where(x => x.PosterID == posterID)
-                    .Include(x =>x.ImagePoster)
-                    .FirstOrDefaultAsync();
+                var poster = await _context.Posters
+                    .Include(x => x.ImagePoster)
+                    .FirstOrDefaultAsync(x => x.PosterID == posterID);
 
-                if (getData != null)
-                {
-                    _context.Posters .Remove(getData);
-                    await _context.SaveChangesAsync();
+                if (poster == null)
+                    throw new Exception("Poster not found.");
 
-                    return ServiceResult<bool>.SuccessResult(true);
-                }
-                throw new Exception("Not Found or Data don't exist");
+                _context.Posters.Remove(poster);
+                await _context.SaveChangesAsync();
+
+                return ServiceResult<bool>.SuccessResult(true);
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Error deleting poster.");
                 return ServiceResult<bool>.FailedResult(ex.Message);
             }
         }
